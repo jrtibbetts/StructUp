e0d53c28f5b90b41157d1cc92a43e0375c7844d8
Jason R Tibbetts (NRitH) <jason@tibbetts.net>
2019-05-20 17:36:40 -0400

    Initial Commit

---
+++ StructUp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StructUp
+//
+//  Created by Tibbetts, Jason on 5/20/19.
+//  Copyright © 2019 Poikile Creations. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
